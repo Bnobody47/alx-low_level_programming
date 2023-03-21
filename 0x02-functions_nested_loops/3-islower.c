1043938c3f39d086bcbaeb52132877b8d7a1488b
Bnobody47 <beamlakadaneb@gmail.com>
2023-03-21 11:18:18 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * -islower - Entry point
+ *  Return: Always 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
