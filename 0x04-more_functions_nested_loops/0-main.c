e1d6dbb3d9b0b9b72d24f3c00f3d11d9dd2c06b1
Bnobody47 <beamlakadaneb@gmail.com>
2023-03-23 09:59:56 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -3,7 +3,7 @@
 
 /**
  * main - check the code.
- *
+ * @c: Character check
  * Return: Always 0.
  */
 int main(void)
@@ -14,5 +14,6 @@ int main(void)
 	printf("%c: %d\n", c, _isupper(c));
 	c = 'a';
 	printf("%c: %d\n", c, _isupper(c));
+
 	return (0);
 }
