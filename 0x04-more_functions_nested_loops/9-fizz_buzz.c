0087361207e57ca95b7d1bfd20c342e2e8acea04
Bnobody47 <beamlakadaneb@gmail.com>
2023-03-23 14:10:41 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -19,10 +19,10 @@ int main(void)
 			printf("Buzz");
 		else
 			printf("%d", i);
-		if (i == 100)
-			continue;
-		printf(" ");
+		if (i < 100)
+			printf(" ");
 	}
+
 	printf("\n");
 
 	return (0);
