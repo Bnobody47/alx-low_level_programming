cd45534700cb9ae222452487087f1dae58982192
Bnobody47 <beamlakadaneb@gmail.com>
2023-03-27 13:18:00 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copies a string
+ * @dest: A buffer to copy
+ * @src: The source string
+ * Return: A pointer to the destination
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int index = 0;
+
+	while (src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
+	return (dest);
+}
