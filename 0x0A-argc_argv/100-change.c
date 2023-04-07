890bc1a45445c2bdfd15b0049a078fb27c1143b0
Bnobody47 <beamlakadaneb@gmail.com>
2023-04-07 10:34:31 -0700

    Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,40 +11,39 @@
 
 int main(int argc, char *argv[])
 {
-	int coins = 0;
-	int cash;
+	int sum, count;
+	unsigned int i;
+	char *p;
+	int cent[] = {25, 10, 5, 2};
 
-	if (argc == 2)
+	if (argc != 2)
 	{
-		if (strchr(argv[argc - 1], '-'))
-		{
-			printf("0\n");
-			return (1);
-		}
-		cash = atoi(argv[argc - 1]);
-		while (cash > 0)
-		{
-			if (cash % 25 == 0)
-			{
-				cash -= 25;
-			} else if (cash % 10 == 0)
-			{
-				cash -= 10;
-			} else if (cash % 5 == 0)
-			{
-				cash -= 5;
-			} else if (cash % 2 == 0)
-			{
-				cash -= 2;
-			} else
-			{
-				cash--;
-			}
-			coins++;
-		}
-		printf("%d\n", coins);
-		return (0);
+		printf("Error\n");
+		return (1);
 	}
-	printf("Error\n");
-	return (1);
+	sum = strtol(argv[1], &p, 10);
+	count = 0;
+	if (!*p)
+	{
+		while (sum > 1)
+		{
+			for (i = 0; i < sizeof(cent[i]); i++)
+			{
+				if (sum >= cent[i])
+				{
+					count += sum / cent[i];
+					sum = sum % cent[i];
+				}
+			}
+		}
+		if (sum == 1)
+		count++;
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", count);
+	return (0);
 }
