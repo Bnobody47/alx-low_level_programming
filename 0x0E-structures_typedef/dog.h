f3093d4f8b0c5e75d505c0ee5378cc4f69df510c
Bnobody47 <beamlakadaneb@gmail.com>
2023-04-17 14:04:59 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - structure of the dog
+ * @name: dogs name
+ * @age: age of the dog
+ * @owner: dogs owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
