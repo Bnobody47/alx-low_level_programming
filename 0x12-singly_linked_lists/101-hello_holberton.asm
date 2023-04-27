d4b09c4d717adb14d131bd06ffdfca9ec65c2599
Bnobody47 <beamlakadaneb@gmail.com>
2023-04-27 12:50:55 -0700

    Real programmers can write assembly code in any language

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,27 +1,19 @@
-global  main
-extern  printf
-
 section .data
-    hello_string db 'Hello, Holberton', 10, 0 ; The string to be printed followed by a newline and a null terminator
+hello:		db "Hello, Holberton",10,0
+format:		db "%s",0
 
-section .text
+	    section .text
+	    global main
+	    extern printf
+
 main:
-    ; Save registers
-    push    rbp
-    mov     rbp, rsp
+	    push rbp
+	    mov rbp, rsp
 
-    ; Prepare the arguments for printf
-    lea     rdi, [hello_string] ; Load the address of the hello_string into the first argument (rdi)
-    xor     rax, rax            ; Set rax to 0 (printf is a variadic function, so rax must be initialized to the number of vector registers used)
+	    mov rsi, hello
+	    mov rdi, format
+	    call printf
 
-    ; Call printf
-    call    printf
-
-    ; Restore registers
-    mov     rsp, rbp
-    pop     rbp
-
-    ; Exit the program
-    xor     rdi, rdi ; Set the exit code to 0
-    mov     rax,60  ; Set rax to the exit syscall number (60 for x86_64)
-    syscall         ; Call the exit syscall
+	    pop rbp
+	    mov rax, 0
+	    ret
