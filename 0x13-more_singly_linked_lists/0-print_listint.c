2336a0ce6e61816e794cdc935534787f8b9c7c22
Bnobody47 <beamlakadaneb@gmail.com>
2023-05-02 07:28:02 -0700

    Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,14 +11,16 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 0;
+	int nodes = 0;
 
-	while (h)
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
 	{
-		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
+		nodes++;
 	}
-
 	return (nodes);
 }
